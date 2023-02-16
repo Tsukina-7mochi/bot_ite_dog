4c24f9cabc9b81f34696b5b3e0bd0231d14db320
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-02-16 20:17:48 +0900

    implement discord bot

---
+++ src/bot.py
@@ -0,0 +1,43 @@
+from generate import generate
+import sys
+from typing import Optional
+import discord
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("token")
+    parser.add_argument("--sync-command", action="store_true")
+    args = parser.parse_args()
+
+    token = args.token
+
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    client = discord.Client(intents=intents)
+    tree = discord.app_commands.CommandTree(client)
+
+    @tree.command(name="botdog", description="let the dog mutter")
+    @discord.app_commands.describe(first_word="first word")
+    async def bot_dog_command(interaction, first_word: Optional[str]):
+        if first_word == None:
+            generated = generate("", "../text/dest/ngram.txt", 3, "^", "$")
+        else:
+            generated = generate(first_word, "../text/dest/ngram.txt", 3, "^", "$")
+
+        await interaction.response.send_message(generated)
+
+    @client.event
+    async def on_ready():
+        print(f"Logged on as {client.user}")
+
+        if args.sync_command:
+            print("Syncing slash commands...", end="")
+            await tree.sync()
+            print("complete")
+
+    client.run(token)
+
+if __name__ == "__main__":
+    main()
